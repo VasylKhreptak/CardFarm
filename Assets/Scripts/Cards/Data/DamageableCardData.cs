13641aed1b8c05dde3bed07147483e9e6249045c
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-13 15:51:17 +0300

    Updated DamageableCardData;

--- Assets/Scripts/Cards/Data/DamageableCardData.cs
+++ Assets/Scripts/Cards/Data/DamageableCardData.cs
@@ -6,6 +6,7 @@ namespace Cards.Data
     [Serializable]
     public class DamageableCardData : CardData
     {
+        public IntReactiveProperty MaxHealth = new IntReactiveProperty(5);
         public IntReactiveProperty Health = new IntReactiveProperty(5);
     }
 }
