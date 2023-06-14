9df2559af77d54c2e41d1a98622b322d59c80055
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-14 09:10:23 +0300

    Updated CardData.cs;

--- Assets/Scripts/Cards/Data/CardData.cs
+++ Assets/Scripts/Cards/Data/CardData.cs
@@ -10,5 +10,8 @@ namespace Cards.Data
         public StringReactiveProperty Name = new StringReactiveProperty("Name");
         public ReactiveProperty<Sprite> Background = new ReactiveProperty<Sprite>();
         public ReactiveProperty<Sprite> Icon = new ReactiveProperty<Sprite>();
+        public ReactiveProperty<CardData> TopCard = new ReactiveProperty<CardData>();
+        public ReactiveProperty<CardData> BottomCard = new ReactiveProperty<CardData>();
+        public Transform BottomCardFollowPoint;
     }
 }
