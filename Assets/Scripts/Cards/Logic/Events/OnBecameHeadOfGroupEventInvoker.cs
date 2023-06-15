8fd803f768e5d1d17581859333cce7afec6b4744
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-15 08:47:06 +0300

    Added OnBecameHeadOfGroupEventInvoker

---
+++ Assets/Scripts/Cards/Logic/Events/OnBecameHeadOfGroupEventInvoker.cs
@@ -0,0 +1,34 @@
+﻿using Cards.Data;
+using UnityEngine;
+
+namespace Cards.Logic.Events
+{
+    public class OnBecameHeadOfGroupEventInvoker : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private CardData _cardData;
+
+        #region MonoBehaviour
+
+        private void OnEnable()
+        {
+            _cardData.Callbacks.onGroupCardsListUpdated += OnCardsGroupUpdated;
+        }
+
+        private void OnDisable()
+        {
+            _cardData.Callbacks.onGroupCardsListUpdated -= OnCardsGroupUpdated;
+        }
+
+        #endregion
+
+        private void OnCardsGroupUpdated()
+        {
+            if (_cardData.IsTopCard.Value)
+            {
+                _cardData.Callbacks.onBecameHeadOfGroup?.Invoke();
+                Debug.Log("Became A Head");
+            }
+        }
+    }
+}
