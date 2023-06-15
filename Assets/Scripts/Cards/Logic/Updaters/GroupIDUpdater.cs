af952efe71fb573f0007db52f1c6b5d86492fabf
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-15 15:42:07 +0300

    Added GroupIDUpdater;

---
+++ Assets/Scripts/Cards/Logic/Updaters/GroupIDUpdater.cs
@@ -0,0 +1,55 @@
+﻿using Cards.Data;
+using ItemsIDManagement;
+using UnityEngine;
+using Zenject;
+
+namespace Cards.Logic.Updaters
+{
+    public class GroupIDUpdater : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private CardData _cardData;
+
+        private IDProvider _idProvider;
+
+        [Inject]
+        private void Constructor(IDProvider idProvider)
+        {
+            _idProvider = idProvider;
+        }
+
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            StartObserving();
+        }
+
+        private void OnDestroy()
+        {
+            StoStopObserving();
+        }
+
+        #endregion
+
+        private void StartObserving()
+        {
+            _cardData.Callbacks.onBecameHeadOfGroup += OnBecameHeadOfGroup;
+        }
+
+        private void StoStopObserving()
+        {
+            _cardData.Callbacks.onBecameHeadOfGroup -= OnBecameHeadOfGroup;
+        }
+
+        private void OnBecameHeadOfGroup()
+        {
+            int groupID = _idProvider.Value;
+
+            foreach (var card in _cardData.GroupCards)
+            {
+                card.GroupID.Value = groupID;
+            }
+        }
+    }
+}
