ac1a8e19ffce32719d885131419a309c8ff5b224
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-18 15:39:39 +0300

    Added Base Booster Left Cards Text;

---
+++ Assets/Scripts/Cards/Boosters/Graphics/VisualElements/BoosterLeftCardsText.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Cards.Boosters.Data;
+using TMPro;
+using UniRx;
+using UnityEngine;
+
+namespace Cards.Boosters.Graphics.VisualElements
+{
+    public class BoosterLeftCardsText : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private BoosterCardData _cardData;
+        [SerializeField] private TMP_Text _tmp;
+
+        private IDisposable _leftCardsSubscription;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _tmp ??= GetComponent<TMP_Text>();
+        }
+
+        private void OnEnable()
+        {
+            StartObservingLeftCards();
+        }
+
+        private void OnDisable()
+        {
+            StopObservingLeftCards();
+        }
+
+        #endregion
+
+        private void StartObservingLeftCards()
+        {
+            StopObservingLeftCards();
+            _leftCardsSubscription = _cardData.LeftCards.Subscribe(OnLeftCardsChanged);
+        }
+
+        private void StopObservingLeftCards()
+        {
+            _leftCardsSubscription?.Dispose();
+        }
+
+        private void OnLeftCardsChanged(int leftCards)
+        {
+            _tmp.text = leftCards.ToString();
+        }
+    }
+}
