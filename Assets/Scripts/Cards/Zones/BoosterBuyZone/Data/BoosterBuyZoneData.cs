f60a1f749347780643ef3d82db537c641c8362b7
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-19 16:15:03 +0300

    Added Booster Price and Name Texts;

--- Assets/Scripts/Cards/Zones/BoosterBuyZone/Data/BoosterBuyZoneData.cs
+++ Assets/Scripts/Cards/Zones/BoosterBuyZone/Data/BoosterBuyZoneData.cs
@@ -1,9 +1,10 @@
 ﻿using Cards.Zones.Data;
+using UniRx;
 
 namespace Cards.Zones.BoosterBuyZone.Data
 {
     public class BoosterBuyZoneData : ZoneData
     {
-
+        public IntReactiveProperty BoosterPrice = new IntReactiveProperty(3);
     }
 }
---
+++ Assets/Scripts/Cards/Zones/BoosterBuyZone/Graphics/VisualElements/BoosterPriceText.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Cards.Zones.BoosterBuyZone.Data;
+using TMPro;
+using UniRx;
+using UnityEngine;
+
+namespace Cards.Zones.BoosterBuyZone.Graphics.VisualElements
+{
+    public class BoosterPriceText : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private BoosterBuyZoneData _cardData;
+        [SerializeField] private TMP_Text _tmp;
+
+        private IDisposable _priceSubscription;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _tmp ??= GetComponent<TMP_Text>();
+        }
+
+        private void OnEnable()
+        {
+            StartObservingPrice();
+        }
+
+        private void OnDisable()
+        {
+            StopObservingPrice();
+        }
+
+        #endregion
+
+        private void StartObservingPrice()
+        {
+            StopObservingPrice();
+            _priceSubscription = _cardData.BoosterPrice.Subscribe(SetPrice);
+        }
+
+        private void StopObservingPrice()
+        {
+            _priceSubscription?.Dispose();
+        }
+
+        private void SetPrice(int price)
+        {
+            _tmp.text = price.ToString();
+        }
+    }
+}
