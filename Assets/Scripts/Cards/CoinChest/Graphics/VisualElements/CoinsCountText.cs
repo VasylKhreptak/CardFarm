39c8f5e9b9adab42c6c77931cdc4224cdac4c3ad
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-20 14:58:14 +0300

    Added Coins Count Text;

---
+++ Assets/Scripts/Cards/CoinChest/Graphics/VisualElements/CoinsCountText.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Cards.CoinChest.Data;
+using TMPro;
+using UniRx;
+using UnityEngine;
+
+namespace Cards.CoinChest.Graphics.VisualElements
+{
+    public class CoinsCountText : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private CoinChestCardData _cardData;
+        [SerializeField] private TMP_Text _tmp;
+
+        private IDisposable _coinsCountSubscription;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _tmp ??= GetComponent<TMP_Text>();
+        }
+
+        private void OnEnable()
+        {
+            StartObservingCoinsCount();
+        }
+
+        private void OnDisable()
+        {
+            StopObservingCoinsCount();
+        }
+
+        #endregion
+
+        private void StartObservingCoinsCount()
+        {
+            StopObservingCoinsCount();
+            _coinsCountSubscription = _cardData.Coins.Subscribe(SetCoins);
+        }
+
+        private void StopObservingCoinsCount()
+        {
+            _coinsCountSubscription?.Dispose();
+        }
+
+        private void SetCoins(int coins)
+        {
+            _tmp.text = coins.ToString();
+        }
+    }
+}
