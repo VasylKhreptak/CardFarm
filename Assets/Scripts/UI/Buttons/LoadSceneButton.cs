b48bb00ff6213cbf054cc22ec675fa76ca7d4011
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-26 09:15:26 +0300

    Added Level Restart Button;

---
+++ Assets/Scripts/UI/Buttons/LoadSceneButton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NaughtyAttributes;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI.Buttons
+{
+    public class LoadSceneButton : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Button _button;
+
+        [Header("Preferences")]
+        [SerializeField, Scene] private string _scene;
+
+        private IDisposable _subscription;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _button ??= GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            StartObserving();
+        }
+
+
+        private void OnDisable()
+        {
+            StopObserving();
+        }
+
+        #endregion
+
+        private void StartObserving()
+        {
+            StopObserving();
+            _subscription = _button
+                .OnClickAsObservable()
+                .Subscribe(_ => LoadScene());
+        }
+
+        private void StopObserving()
+        {
+            _subscription?.Dispose();
+        }
+
+        private void LoadScene()
+        {
+            SceneManager.LoadScene(_scene);
+        }
+    }
+}
