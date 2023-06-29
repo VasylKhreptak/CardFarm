0e5536405adc5cf8e13c2492d6c8d9a96f388d51
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-06-29 09:28:42 +0300

    Refactored MoveCardOnAnotherCardQuest;

--- Assets/Scripts/Quests/Logic/QuestObservers/MoveCardOnAnotherCardQuest.cs
+++ Assets/Scripts/Quests/Logic/QuestObservers/MoveCardOnAnotherCardQuest.cs
@@ -1,95 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Cards.Core;
 using Cards.Data;
 using Quests.Logic.QuestObservers.Core;
-using Table.Core;
 using UniRx;
 using UnityEngine;
-using Zenject;
 
 namespace Quests.Logic.QuestObservers
 {
-    public class MoveCardOnAnotherCardQuest : QuestObserver
+    public class MoveCardOnAnotherCardQuest : QuestCardsObserver
     {
         [Header("Preferences")]
-        [SerializeField] private Card _topCard;
-        [SerializeField] private Card _bottomCard;
+        [SerializeField] private Card _targetTopCard;
 
-        private IDisposable _newCardsAppearedSubscription;
-        private IDisposable _topCardsCountSubscription;
         private IDisposable _topCardsSubscription;
-
-        private CardsTableSelector _cardsTableSelector;
-
-        [Inject]
-        private void Constructor(CardsTableSelector cardsTableSelector)
-        {
-            _cardsTableSelector = cardsTableSelector;
-        }
-
-        public override void StartObserving()
-        {
-            StartObservingNewCardType();
-        }
 
         public override void StopObserving()
         {
-            StopObservingNewCardType();
-            StopObservingTopCardsCount();
+            base.StopObserving();
+
             StopObservingTopCards();
         }
 
-        private void StartObservingNewCardType()
+        protected override void OnCardsCountChanged(ReactiveCollection<CardData> cards)
         {
-            if (_cardsTableSelector.SelectedCardsMap.TryGetValue(_topCard, out ReactiveCollection<CardData> cards))
-            {
-                StartObservingTopCardsCount(cards);
-            }
-
-            _newCardsAppearedSubscription = _cardsTableSelector.SelectedCardsMap.ObserveAdd()
-                .Subscribe(x =>
-                {
-                    if (x.Key == _topCard)
-                    {
-                        StartObservingTopCardsCount(x.Value);
-                    }
-                });
-        }
-
-        private void StopObservingNewCardType()
-        {
-            _newCardsAppearedSubscription?.Dispose();
-        }
-
-        private void StartObservingTopCardsCount(ReactiveCollection<CardData> cards)
-        {
-            StopObservingTopCardsCount();
-
-            _topCardsCountSubscription = cards.ObserveCountChanged().Subscribe(count =>
-            {
-                StartObservingTopCards(cards);
-            });
-        }
-
-        private void StopObservingTopCardsCount()
-        {
-            _topCardsCountSubscription?.Dispose();
+            StartObservingTopCards(cards);
         }
 
         private void StartObservingTopCards(ReactiveCollection<CardData> cards)
         {
             StopObservingTopCards();
 
-            IObservable<CardData>[] topCardObservables = cards.Select(x => x.TopCard as IObservable<CardData>).ToArray();
+            List<IObservable<CardData>> topCardsObservables = cards.Select(x => x.TopCard as IObservable<CardData>).ToList();
 
-            _topCardsSubscription = Observable.Merge(topCardObservables)
-                .Where(x => x != null && x.Card.Value == _bottomCard)
-                .Subscribe(_ =>
-                {
-                    _questData.IsCompleted.Value = true;
-                    StopObserving();
-                });
+            _topCardsSubscription = topCardsObservables
+                .Merge()
+                .Where(x => x != null && x.Card.Value == _targetTopCard)
+                .Subscribe(_ => MarkQuestAsCompleted());
         }
 
         private void StopObservingTopCards()
