ba308a69431d7341deae0895981a3bc87512fe4e
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-07-18 16:29:46 +0300

    Created Order Spawn Zone;

---
+++ Assets/Scripts/Zenject/Installers/SceneContext/Bounds/UpperZoneBoundsInstaller.cs
@@ -0,0 +1,10 @@
+﻿using Constraints;
+using Plugins.ObjectPooler.Zenject.Installers.SceneContext.Core;
+
+namespace Zenject.Installers.SceneContext
+{
+    public class UpperZoneBoundsInstaller : SceneSingleInstanceInstaller<UpperZoneBounds>
+    {
+
+    }
+}
