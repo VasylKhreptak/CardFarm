91a48f0bfc7fd35102d0cbb9b32ae1f9d2fbfcd1
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-07-18 12:18:25 +0300

    Added Quests;

--- Assets/Scripts/Quests/Logic/Core/Quest.cs
+++ Assets/Scripts/Quests/Logic/Core/Quest.cs
@@ -9,8 +9,14 @@
         BuyHumbleBeginningsBooster = 4,
         MoveWorkerOnWood = 5,
         MakeStickFromWood = 6,
+        BuildGarden = 10,
+        CreateCampfire = 13,
         GrowBerryBush = 7,
         BuildAHouse = 8,
         GrowBaby = 9,
+        BuildQuarry = 11,
+        BuildLumberCamp = 12,
+        CreateBrick = 14,
+        CreatePlank = 15,
     }
 }
