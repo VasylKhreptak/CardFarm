9a5b3f406740e5480784745a4d0243be127372e1
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-07-20 10:59:00 +0300

    Added Coins Text;

---
+++ Assets/Scripts/Graphics/UI/CoinsText.cs
@@ -0,0 +1,56 @@
+﻿using Economy;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace Graphics.UI
+{
+    public class CoinsText : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private TMP_Text _tmp;
+
+        private CoinsBank _coinsBank;
+
+        [Inject]
+        private void Constructor(CoinsBank coinsBank)
+        {
+            _coinsBank = coinsBank;
+        }
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _tmp ??= GetComponent<TMP_Text>();
+        }
+
+        private void OnEnable()
+        {
+            StartObserving();
+        }
+
+        private void OnDisable()
+        {
+            StopObserving();
+        }
+
+        #endregion
+
+        private void StartObserving()
+        {
+            SetText(_coinsBank.Value);
+            _coinsBank.onChanged += SetText;
+        }
+
+        private void StopObserving()
+        {
+            _coinsBank.onChanged -= SetText;
+        }
+
+        private void SetText(int coins)
+        {
+            _tmp.text = coins.ToString();
+        }
+    }
+}
