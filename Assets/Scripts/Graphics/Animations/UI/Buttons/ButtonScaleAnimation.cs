0a4e712d39f937eb65ceb3e18c421e07bdc08d3e
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-07-25 10:01:41 +0300

    Added ButtonScaleAnimation

---
+++ Assets/Scripts/Graphics/Animations/UI/Buttons/ButtonScaleAnimation.cs
@@ -0,0 +1,71 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Graphics.Animations.UI.Buttons
+{
+    public class ButtonScaleAnimation : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        [Header("References")]
+        [SerializeField] private Button _button;
+
+        [Header("Preferences")]
+        [SerializeField] private Vector3 _initialScale = Vector3.one;
+        [SerializeField] private Vector3 _pressedScale = Vector3.one * 0.9f;
+        [SerializeField] private float _duration = 0.5f;
+        [SerializeField] private AnimationCurve _curve;
+
+        private Tween _scaleTween;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _button ??= GetComponent<Button>();
+        }
+
+        private void Awake()
+        {
+            _initialScale = transform.localScale;
+        }
+
+        private void OnDisable()
+        {
+            ResetScale();
+            KillAnimation();
+        }
+
+        #endregion
+
+        public void OnPointerDown(PointerEventData data)
+        {
+            SetScaleSmooth(_pressedScale);
+        }
+
+        public void OnPointerUp(PointerEventData data)
+        {
+            SetScaleSmooth(_initialScale);
+        }
+
+        private void SetScaleSmooth(Vector3 scale)
+        {
+            KillAnimation();
+
+            _scaleTween = transform
+                .DOScale(scale, _duration)
+                .SetEase(_curve)
+                .Play();
+        }
+
+        private void ResetScale()
+        {
+            transform.localScale = _initialScale;
+        }
+
+        private void KillAnimation()
+        {
+            _scaleTween?.Kill();
+        }
+    }
+}
