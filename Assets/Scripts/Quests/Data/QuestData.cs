9d79efed9ae612436bc8ce87e7b8e1a9a1778524
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-02 14:21:04 +0300

    Added QuestTutorialExecutor.cs;

--- Assets/Scripts/Quests/Data/QuestData.cs
+++ Assets/Scripts/Quests/Data/QuestData.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using Quests.Logic.Core;
+using Quests.Logic.Tutorial.Core;
 using UniRx;
 using UnityEngine;
+using Zenject;
 
 namespace Quests.Data
 {
     [Serializable]
-    public class QuestData : MonoBehaviour
+    public class QuestData : MonoBehaviour, IValidatable
     {
         public Quest Quest;
         public string Name;
@@ -15,7 +17,21 @@ namespace Quests.Data
         public BoolReactiveProperty TookReward = new BoolReactiveProperty();
         public QuestRecipe Recipe = new QuestRecipe();
         public FloatReactiveProperty Progress = new FloatReactiveProperty();
-
+        public QuestTutorialExecutor QuestTutorialExecutor;
         public QuestCallbacks Callbacks = new QuestCallbacks();
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            Validate();
+        }
+
+        public void Validate()
+        {
+            QuestTutorialExecutor = GetComponentInChildren<QuestTutorialExecutor>();
+        }
+
+        #endregion
     }
 }
---
+++ Assets/Scripts/Quests/Logic/Tutorial/Core/QuestTutorialExecutor.cs
@@ -0,0 +1,24 @@
+﻿using Graphics.UI.Panels;
+using Graphics.UI.VisualElements;
+using UnityEngine;
+using Zenject;
+
+namespace Quests.Logic.Tutorial.Core
+{
+    public abstract class QuestTutorialExecutor : MonoBehaviour
+    {
+        protected TutorialHand _tutorialHand;
+        protected TutorialTextPanel _tutorialTextPanel;
+
+        [Inject]
+        private void Constructor(TutorialHand tutorialHand, TutorialTextPanel tutorialTextPanel)
+        {
+            _tutorialHand = tutorialHand;
+            _tutorialTextPanel = tutorialTextPanel;
+        }
+
+        public abstract void StartTutorial();
+
+        public abstract void StopTutorial();
+    }
+}
