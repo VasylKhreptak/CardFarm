93f1ceeac90d40b430505e753a8c62eda0733476
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-04 14:03:06 +0300

    Changed gears appear logic;

---
+++ Assets/Scripts/Cards/Graphics/Logic/RecipeExecutionItemDrawer.cs
@@ -0,0 +1,105 @@
+﻿using Cards.Data;
+using Cards.Factories.Data;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Cards.Graphics.Logic
+{
+    public class RecipeExecutionItemDrawer : MonoBehaviour, IValidatable
+    {
+        [Header("References")]
+        [SerializeField] private CardData _cardData;
+
+        [Header("Preferences")]
+        [SerializeField] private GameObject _targetObject;
+
+        private CompositeDisposable _subscriptions = new CompositeDisposable();
+
+        private FactoryData _factoryData;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            Validate();
+        }
+
+        public void Validate()
+        {
+            _cardData = GetComponentInParent<CardData>(true);
+        }
+
+        private void Awake()
+        {
+            if (_cardData.IsAutomatedFactory)
+            {
+                _factoryData = _cardData as FactoryData;
+            }
+        }
+
+        private void OnEnable()
+        {
+            StartObserving();
+        }
+
+        private void OnDisable()
+        {
+            StopObserving();
+        }
+
+        #endregion
+
+        private void StartObserving()
+        {
+            _cardData.RecipeExecutor.IsExecuting.Subscribe(_ => OnEnvironmentChanged()).AddTo(_subscriptions);
+            _cardData.ReproductionLogic.IsExecuting.Subscribe(_ => OnEnvironmentChanged()).AddTo(_subscriptions);
+            _cardData.IsAnyGroupCardSelected.Subscribe(_ => OnEnvironmentChanged()).AddTo(_subscriptions);
+
+            if (_factoryData != null)
+            {
+                _factoryData.FactoryRecipeExecutor.IsExecuting.Subscribe(_ => OnEnvironmentChanged()).AddTo(_subscriptions);
+            }
+        }
+
+        private void StopObserving()
+        {
+            _subscriptions?.Clear();
+        }
+
+        private void OnEnvironmentChanged()
+        {
+            bool isExecuting = _cardData.RecipeExecutor.IsExecuting.Value || _cardData.ReproductionLogic.IsExecuting.Value;
+
+            if (_factoryData != null && _factoryData.FactoryRecipeExecutor.IsExecuting.Value)
+            {
+                isExecuting = true;
+            }
+
+            if (_cardData.IsAnyGroupCardSelected.Value)
+            {
+                Hide();
+                return;
+            }
+
+            if (isExecuting)
+            {
+                Show();
+            }
+            else
+            {
+                Hide();
+            }
+        }
+
+        private void Show()
+        {
+            _targetObject.SetActive(true);
+        }
+
+        private void Hide()
+        {
+            _targetObject.SetActive(false);
+        }
+    }
+}
