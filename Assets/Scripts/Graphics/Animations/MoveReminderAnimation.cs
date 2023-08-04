5c38a731e95a0f7f6b1a623555d70696b3864b8c
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-04 16:25:44 +0300

    Added MoveReminderAnimation;

---
+++ Assets/Scripts/Graphics/Animations/MoveReminderAnimation.cs
@@ -0,0 +1,66 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Graphics.Animations
+{
+    public class MoveReminderAnimation : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Transform _transform;
+
+        [Header("Preferences")]
+        [SerializeField] private Vector3 _startPosition;
+        [SerializeField] private Vector3 _endPosition;
+        [SerializeField] private float _duration = 1f;
+        [SerializeField] private float _delay = 1f;
+        [SerializeField] private AnimationCurve _moveCurve;
+
+        private Sequence _sequence;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _transform ??= GetComponent<Transform>();
+        }
+
+        private void OnEnable()
+        {
+            StartAnimation();
+        }
+
+        private void OnDisable()
+        {
+            StopAnimation();
+        }
+
+        #endregion
+
+        private void StartAnimation()
+        {
+            StopAnimation();
+
+            _sequence = DOTween.Sequence();
+
+            _sequence
+                .AppendInterval(_delay)
+                .Append(CreateMoveTween(_endPosition))
+                .Append(CreateMoveTween(_startPosition))
+                .SetLoops(-1)
+                .Play();
+        }
+
+        private Tween CreateMoveTween(Vector3 targetPosition)
+        {
+            return _transform
+                .DOMove(targetPosition, _duration)
+                .SetEase(_moveCurve);
+        }
+
+        private void StopAnimation()
+        {
+            _sequence?.Kill();
+            _transform.position = _startPosition;
+        }
+    }
+}
