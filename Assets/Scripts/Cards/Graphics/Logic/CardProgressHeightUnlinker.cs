8bad34a83f4001449e4a40a1caf0829ca8fde268
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-07 10:09:31 +0300

    Temporary unlinked card progressbar;

---
+++ Assets/Scripts/Cards/Graphics/Logic/CardProgressHeightUnlinker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Cards.Graphics.Logic.ProgressHeightUpdaters.Core;
+using UniRx;
+using Zenject;
+
+namespace Cards.Graphics.Logic
+{
+    public class CardProgressHeightUnlinker : BaseCardProgressHeightUnlinker, IValidatable
+    {
+        private IDisposable _isRecipeExecutingSubscription;
+
+        #region MonoBehaviour
+
+        private void OnEnable()
+        {
+            StartObserving();
+        }
+
+        protected override void OnDisable()
+        {
+            StopObserving();
+
+            base.OnDisable();
+        }
+
+        #endregion
+
+        private void StartObserving()
+        {
+            StopObserving();
+
+            _isRecipeExecutingSubscription = _cardData.RecipeExecutor.IsExecuting.Subscribe(isExecuting =>
+            {
+                if (isExecuting)
+                {
+                    StartUnlinking();
+                }
+                else
+                {
+                    StopUnlinking();
+                }
+            });
+        }
+
+        private void StopObserving()
+        {
+            _isRecipeExecutingSubscription?.Dispose();
+        }
+    }
+}
