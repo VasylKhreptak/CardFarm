9556e063ed6538f54bef8850f8c84fea23c0bd41
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-10 11:25:42 +0300

    Updated Cards Background;

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,4 +1,5 @@
 using Graphics.UI.Particles.Coins.Logic;
+using TMPro;
 using UniRx;
 using UnityEngine;
 using Zenject;
@@ -19,33 +20,5 @@ public class Test : MonoBehaviour
 
     private void Awake()
     {
-        // ReactiveCollection<int> collection = new ReactiveCollection<int>();
-        //
-        // collection.ObserveAdd().Subscribe(value => Debug.Log($"Add: {value}")).AddTo(this);
-        // collection.ObserveRemove().Subscribe(value => Debug.Log($"Remove: {value}")).AddTo(this);
-        // collection.ObserveReset().Subscribe(value => Debug.Log($"Reset")).AddTo(this);
-        //
-        // collection.Add(1);
-        // collection.Add(2);
-        // collection.Remove(1);
-        // collection.Clear();
-
     }
-
-    // private void Update()
-    // {
-    //     if (Input.GetMouseButtonDown(0))
-    //     {
-    //         Vector3 startPosition = Input.mousePosition;
-    //
-    //         _coinsCollector.Collect(Random.Range(1, 21), startPosition);
-    //     }
-    //
-    //     if (Input.GetMouseButtonDown(2))
-    //     {
-    //         Vector3 startPosition = Input.mousePosition;
-    //
-    //         _coinsSpender.Spend(Random.Range(1, 21), startPosition);
-    //     }
-    // }
 }
