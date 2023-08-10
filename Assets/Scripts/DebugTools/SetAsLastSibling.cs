8ecbbd17a9cc4a1bee0a6d56c5eca3d2d2cac984
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-10 11:42:21 +0300

    Updated Card Graphics;

---
+++ Assets/Scripts/DebugTools/SetAsLastSibling.cs
@@ -0,0 +1,14 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace DebugTools
+{
+    public class SetAsLastSibling : MonoBehaviour
+    {
+        [Button("Set As Last Sibling")]
+        private void Set()
+        {
+            transform.SetAsLastSibling();
+        }
+    }
+}
