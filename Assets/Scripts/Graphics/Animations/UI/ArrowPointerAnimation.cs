0227dcaffc97883c85c36b00d6640dfb2520fb8e
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-17 13:10:15 +0300

    Added Get Reward Panel Arrow pointer;

---
+++ Assets/Scripts/Graphics/Animations/UI/ArrowPointerAnimation.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using DG.Tweening;
+using NaughtyAttributes;
+using UniRx;
+using UnityEngine;
+
+namespace Graphics.Animations.UI
+{
+    public class ArrowPointerAnimation : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private RectTransform _rectTransform;
+        [SerializeField] private CanvasGroup _canvasGroup;
+
+        [Header("Preferences")]
+        [SerializeField] private float _generalDuration;
+        [SerializeField] private float _playDelay;
+
+        [Header("Fade Preferences")]
+        [SerializeField] private float _startAlpha;
+        [SerializeField] private float _endAlpha;
+        [SerializeField] private float _fadeDuration;
+        [SerializeField] private AnimationCurve _fadeCurve;
+
+        [Header("Anchor Move Preferences")]
+        [SerializeField] private Vector2 _startAnchorPosition;
+        [SerializeField] private Vector2 _endAnchorPosition;
+        [SerializeField] private AnimationCurve _moveCurve;
+
+        [Header("Scale Preferences")]
+        [SerializeField] private float _scaleDelay = 0.3f;
+        [SerializeField] private Vector3 _startScale = Vector3.one;
+        [SerializeField] private Vector3 _endScale = Vector3.one;
+        [SerializeField] private AnimationCurve _scaleCurve;
+
+        private IDisposable _delaySubscription;
+
+        private Sequence _sequence;
+        private Tween _fadeTween;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _rectTransform ??= GetComponent<RectTransform>();
+        }
+
+        private void OnDisable()
+        {
+            KillSequences();
+            ResetValues();
+            _delaySubscription?.Dispose();
+        }
+
+        #endregion
+
+
+        [Button()]
+        public void Play()
+        {
+            _delaySubscription?.Dispose();
+            _delaySubscription = Observable.Timer(TimeSpan.FromSeconds(_playDelay)).Subscribe(_ =>
+            {
+                KillSequences();
+                ResetValues();
+
+                gameObject.SetActive(true);
+
+                _fadeTween = _canvasGroup
+                    .DOFade(_endAlpha, _fadeDuration)
+                    .SetEase(_fadeCurve)
+                    .Play();
+
+                _sequence = DOTween.Sequence();
+
+                _sequence
+                    .Join(_rectTransform.DOAnchorPos(_endAnchorPosition, _generalDuration).SetEase(_moveCurve))
+                    .Join(_rectTransform.DOScale(_endScale, _generalDuration).SetEase(_scaleCurve).SetDelay(_scaleDelay))
+                    .SetLoops(-1, LoopType.Yoyo)
+                    .Play();
+            });
+        }
+
+        [Button()]
+        public void Stop()
+        {
+            _delaySubscription?.Dispose();
+            if (gameObject.activeSelf == false) return;
+
+            KillSequences();
+
+            _fadeTween = _canvasGroup
+                .DOFade(_startAlpha, _fadeDuration)
+                .SetEase(_fadeCurve)
+                .OnComplete(() => gameObject.SetActive(false))
+                .Play();
+        }
+
+        private void KillSequences()
+        {
+            _sequence?.Kill();
+            _fadeTween?.Kill();
+        }
+
+        private void ResetValues()
+        {
+            _canvasGroup.alpha = _startAlpha;
+            _rectTransform.anchoredPosition = _startAnchorPosition;
+            _rectTransform.localScale = _startScale;
+        }
+
+        [Button()]
+        private void AssignStartAnchorPosition()
+        {
+            _startAnchorPosition = _rectTransform.anchoredPosition;
+        }
+
+        [Button()]
+        private void AssignStartScale()
+        {
+            _startScale = _rectTransform.localScale;
+        }
+
+        [Button()]
+        private void AssignEndAnchorPosition()
+        {
+            _endAnchorPosition = _rectTransform.anchoredPosition;
+        }
+
+        [Button()]
+        private void AssignEndScale()
+        {
+            _endScale = _rectTransform.localScale;
+        }
+    }
+}
---
+++ Assets/Scripts/Graphics/UI/VisualElements/GetRewardArrowPointer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Graphics.Animations.UI;
+using Quests.Logic;
+using Runtime.Commands;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Graphics.UI.VisualElements
+{
+    public class GetRewardArrowPointer : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private ArrowPointerAnimation _arrowPointerAnimation;
+
+        private IDisposable _completedQuestsSubscription;
+
+        private QuestsManager _questsManager;
+        private GameRestartCommand _gameRestartCommand;
+
+        [Inject]
+        private void Constructor(QuestsManager questsManager, GameRestartCommand gameRestartCommand)
+        {
+            _questsManager = questsManager;
+            _gameRestartCommand = gameRestartCommand;
+        }
+
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            _gameRestartCommand.OnExecute += OnRestart;
+        }
+
+        private void OnValidate()
+        {
+            _arrowPointerAnimation ??= GetComponentInChildren<ArrowPointerAnimation>(true);
+        }
+
+        private void OnEnable()
+        {
+            StartObservingQuests();
+        }
+
+        private void OnDisable()
+        {
+            StopObservingQuests();
+        }
+
+        private void OnDestroy()
+        {
+            _gameRestartCommand.OnExecute -= OnRestart;
+        }
+
+        #endregion
+
+        private void StartObservingQuests()
+        {
+            StopObservingQuests();
+
+            _completedQuestsSubscription = _questsManager.CompletedQuests
+                .ObserveCountChanged()
+                .DoOnSubscribe(OnCompletedQuestsChanged)
+                .Subscribe(_ => OnCompletedQuestsChanged());
+        }
+
+        private void StopObservingQuests()
+        {
+            _completedQuestsSubscription?.Dispose();
+        }
+
+        private void OnCompletedQuestsChanged()
+        {
+            int completedQuestsCount = _questsManager.CompletedQuests.Count;
+
+            if (completedQuestsCount == 1)
+            {
+                _arrowPointerAnimation.Play();
+            }
+            else
+            {
+                _arrowPointerAnimation.Stop();
+            }
+        }
+
+        private void OnRestart()
+        {
+            _arrowPointerAnimation.gameObject.SetActive(false);
+        }
+    }
+}
