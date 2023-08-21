2dfed02b6fbadff70d5401f8106bc12c856045ab
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-21 14:37:02 +0300

    Added GearsDataReseter;

---
+++ Assets/Scripts/Graphics/VisualElements/Gears/Logic/GearsDataReseter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Graphics.VisualElements.Gears.Logic
+{
+    public class GearsDataReseter : MonoBehaviour, IValidatable
+    {
+        [Header("References")]
+        [SerializeField] private GearsData _gearsData;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            Validate();
+        }
+
+        public void Validate()
+        {
+            _gearsData = GetComponentInParent<GearsData>(true);
+        }
+
+        private void OnDisable()
+        {
+            ResetData();
+        }
+
+        #endregion
+
+        private void ResetData()
+        {
+            _gearsData.ShowAnimation.MoveToStartState();
+            _gearsData.GearsShowAnimation.MoveToEndState();
+            _gearsData.MarkHideAnimation.MoveToEndState();
+        }
+    }
+}
