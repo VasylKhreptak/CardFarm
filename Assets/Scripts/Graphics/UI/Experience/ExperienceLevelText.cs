99faa7954941354fd38fb2aeb17bbaf305505ac8
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-25 16:37:48 +0300

    Added Experience Visualization;

---
+++ Assets/Scripts/Graphics/UI/Experience/ExperienceLevelText.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Data.Player.Core;
+using Data.Player.Experience;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Graphics.UI.Experience
+{
+    public class ExperienceLevelText : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private TMP_Text _tmp;
+
+        private IDisposable _experienceLevelSubscription;
+
+        private ExperienceData _experienceData;
+
+        [Inject]
+        private void Constructor(PlayerData playerData)
+        {
+            _experienceData = playerData.ExperienceData;
+        }
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _tmp ??= GetComponent<TMP_Text>();
+        }
+
+        private void OnEnable()
+        {
+            StartObserving();
+        }
+
+        private void OnDisable()
+        {
+            StopObserving();
+        }
+
+        #endregion
+
+        private void StartObserving()
+        {
+            StopObserving();
+
+            _experienceLevelSubscription = _experienceData.ExperienceLevel.Subscribe(SetText);
+        }
+
+        private void StopObserving()
+        {
+            _experienceLevelSubscription?.Dispose();
+        }
+
+        private void SetText(int experienceLevel)
+        {
+            _tmp.text = experienceLevel.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Graphics/UI/Experience/ExperienceProgress.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Data.Player.Core;
+using Data.Player.Experience;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Graphics.UI.Experience
+{
+    public class ExperienceProgress : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Slider _slider;
+
+        [Header("Preferences")]
+        [SerializeField] private float _duration = 0.5f;
+        [SerializeField] private AnimationCurve _curve;
+
+        private IDisposable _fillAmountSubscription;
+
+        private Tween _tween;
+
+        private ExperienceData _experienceData;
+
+        [Inject]
+        private void Constructor(PlayerData playerData)
+        {
+            _experienceData = playerData.ExperienceData;
+        }
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _slider ??= GetComponent<Slider>();
+        }
+
+        private void OnEnable()
+        {
+            SetSliderValue(_experienceData.FillAmount.Value);
+            StartObserving();
+        }
+
+        private void OnDisable()
+        {
+            StopObserving();
+            StopUpdatingSliderValue();
+        }
+
+        #endregion
+
+        private void StartObserving()
+        {
+            StopObserving();
+            _fillAmountSubscription = _experienceData.FillAmount
+                .Subscribe(SetSliderValueSmooth);
+        }
+
+        private void StopObserving()
+        {
+            _fillAmountSubscription?.Dispose();
+        }
+
+        private void SetSliderValueSmooth(float value)
+        {
+            StopUpdatingSliderValue();
+
+            _tween = DOTween
+                .To(GetSliderValue, SetSliderValue, value, _duration)
+                .SetEase(_curve)
+                .Play();
+        }
+
+        private float GetSliderValue() => _slider.value;
+
+        private void SetSliderValue(float value) => _slider.value = value;
+
+        private void StopUpdatingSliderValue()
+        {
+            _tween?.Kill();
+        }
+    }
+}
