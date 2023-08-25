54134fb5ba52c827e7825113c7923cb46109ccb3
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-25 15:16:34 +0300

    Added Experience Bank Installer;

---
+++ Assets/Scripts/Data/Player/Experience/Logic/ExperienceBank.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Data.Player.Experience.Logic
+{
+    public class ExperienceBank : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Zenject/Installers/SceneContext/Economy/ExperienceBankInstaller.cs
@@ -0,0 +1,10 @@
+﻿using Data.Player.Experience.Logic;
+using Plugins.ObjectPooler.Zenject.Installers.SceneContext.Core;
+
+namespace Zenject.Installers.SceneContext.Economy
+{
+    public class ExperienceBankInstaller : SceneSingleInstanceInstaller<ExperienceBank>
+    {
+        
+    }
+}
