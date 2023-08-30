3f3d20837680d9ae5d948be6304e870b870324ba
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-08-30 16:33:34 +0300

    Added OpenShopButton;

--- Assets/Scripts/Shop/Buttons/OpenShopButton.cs
+++ Assets/Scripts/Shop/Buttons/OpenShopButton.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace Shop.Buttons
 {
@@ -8,6 +9,31 @@ namespace Shop.Buttons
         [Header("References")]
         [SerializeField] private Button _button;
 
+        private ShopPanel _shopPanel;
 
+        [Inject]
+        private void Constructor(ShopPanel shopPanel)
+        {
+            _shopPanel = shopPanel;
+        }
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _button ??= GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(_shopPanel.Show);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(_shopPanel.Show);
+        }
+
+        #endregion
     }
 }
--- Assets/Scripts/Shop/ShopPanel.cs
+++ Assets/Scripts/Shop/ShopPanel.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 namespace Shop
 {
@@ -6,15 +7,40 @@ namespace Shop
     {
         [Header("References")]
         [SerializeField] private GameObject _shopObject;
+        [SerializeField] private CanvasGroup _canvasGroup;
+
+        [Header("Animation Preferences")]
+        [SerializeField] private float _fadeDuration = 0.4f;
+
+        private Tween _animation;
+
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            _canvasGroup.alpha = 0f;
+        }
+
+        private void OnDisable()
+        {
+            KillAnimation();
+        }
+
+        #endregion
 
         public void Show()
         {
             _shopObject.SetActive(true);
+            KillAnimation();
+            _animation = _canvasGroup.DOFade(1f, _fadeDuration).Play();
         }
 
         public void Hide()
         {
-            _shopObject.SetActive(false);
+            KillAnimation();
+            _animation = _canvasGroup.DOFade(0f, _fadeDuration).OnComplete(() => _shopObject.SetActive(false)).Play();
         }
+
+        private void KillAnimation() => _animation?.Kill();
     }
 }
